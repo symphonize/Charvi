76392696484e56e8d88bb1f9af44620a6507a972
Ashok Cherukuri <ashok@switch.(none)>
2012-08-30 18:07:03 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charvi::Application.config.secret_token = 'ad85ef71e50a3484b928ebb116a6a95f6b8c79a5a8deeba392a66c4cf27e87aede1829fab7056cd95a71766431813e6bdf79be83a5df82f081ce8f1403bbecc5'
